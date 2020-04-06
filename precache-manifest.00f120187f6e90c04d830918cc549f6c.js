ebd65bae1b1d6140d356ba86639e42cce7852126
rajan savaliya <rajansavaliya6328@gmail.com>
2020-04-06 18:18:48 +0530

    Updates

---
+++ precache-manifest.00f120187f6e90c04d830918cc549f6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39cdcc4a2e3542f721728209e0aeab96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a38861a672e194b7d9b8",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a30e6b66c6906818d1c5",
+    "url": "/static/js/2.82aebd89.chunk.js"
+  },
+  {
+    "revision": "a38861a672e194b7d9b8",
+    "url": "/static/js/main.771b3612.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
