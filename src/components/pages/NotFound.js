be654da29bd7ffd625e23fb4326f6e5b06c767e1
rajan savaliya <rajansavaliya6328@gmail.com>
2020-04-26 13:15:24 +0530

    this is a about chage iddt

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -3,8 +3,9 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div>
-      <h1>Not Found</h1>
-      <p className='lead'>The page you are looking for does not exist...</p>
+      <h1>Not Found Pag 404</h1>
+      <p className='lead'>TThis page is not avaoible right now</p>
+      <p> this page not ablie so please go to the main page and go from that place </p>
     </div>
   );
 };
