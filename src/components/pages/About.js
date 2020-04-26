c4e2814ab7ec02ff213a56bc315a5bd9ff6d4563
rajan savaliya <rajansavaliya6328@gmail.com>
2020-04-26 13:12:25 +0530

    this is a about chage iddt

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,10 +3,10 @@ import React, { Fragment } from 'react';
 const About = () => {
   return (
     <Fragment>
-    
+
       <h1>About This App</h1>
-      <p>Search github profile and find anything you want</p>
-      <p>Version: 1.0.0</p>
+      <p>Search github all about about page profile and find anything you want</p>
+      <p>Version: 1.0.1</p>
     </Fragment>
   );
 };
